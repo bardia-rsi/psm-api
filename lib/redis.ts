dd798677ad8b0150215185fc83396aa9663cff0a
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-08-22 16:59:17 +0330

    Implement the redis connection

---
+++ lib/redis.ts
@@ -0,0 +1,9 @@
+import { Redis } from "ioredis";
+
+const redis = new Redis();
+
+redis.on("connect", () => console.log("Connection has been established successfully to the Redis."));
+
+redis.on("error", (e: any) => console.log("Failed to connect to the Redis redisClient", e));
+
+export default redis;
