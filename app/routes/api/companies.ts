18744e517b1751603252338961248e97f9b73479
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-08-29 18:15:08 +0330

    Add the companies' routes for the API subdomain

---
+++ app/routes/api/companies.ts
@@ -0,0 +1,17 @@
+import { Router } from "express";
+import { json } from "body-parser";
+import { bodyValidator } from "../../../middlewares/bodyValidator";
+import { create, update, remove, get, getAll } from "../../controllers/api/companies";
+
+const router = Router();
+
+router.route("/")
+    .get(getAll)
+    .post([json(), bodyValidator], create);
+
+router.route("/:id")
+    .get(get)
+    .put([json(), bodyValidator], update)
+    .delete(remove);
+
+export default router;
