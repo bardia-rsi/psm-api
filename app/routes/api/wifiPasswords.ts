f61b96e0c9b8b1c1cf56cd6dbb6d21add1c2e938
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-08-29 19:09:37 +0330

    Add the wifi passwords' routes of the API subdomain

---
+++ app/routes/api/wifiPasswords.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { json } from "body-parser";
+import { bodyValidator } from "../../../middlewares/bodyValidator";
+import { create, update, remove, get, getAll, getLength } from "../../controllers/api/wifiPasswords";
+
+const router = Router();
+
+router.route("/")
+    .get(getAll)
+    .post([json(), bodyValidator], create);
+
+router.route("/length")
+    .get(getLength);
+
+router.route("/:id")
+    .get(get)
+    .put([json(), bodyValidator], update)
+    .delete(remove);
+
+export default router;
