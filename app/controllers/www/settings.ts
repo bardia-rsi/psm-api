f653f4715ee3f74b363926aa09ce24103945bc5e
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-01 14:23:41 +0330

    Add the settings controller of the www route

---
+++ app/controllers/www/settings.ts
@@ -0,0 +1,11 @@
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import websiteSettings from "../../../data/www/settings.json";
+
+export const get = (_: Request, res: Response): Response<JSON> => {
+    try {
+        return res.status(StatusCodes.OK).json(websiteSettings);
+    } catch (e) {
+        throw e;
+    }
+}
