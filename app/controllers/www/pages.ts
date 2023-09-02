967dda21ec390c9079ed2d6ed72366f6dc6fbbf8
Bardia Rasouli <bardiarasouli@protonmail.com>
2023-09-02 10:21:09 +0330

    Add the pages controllers of the www routes

---
+++ app/controllers/www/pages.ts
@@ -0,0 +1,30 @@
+import type { Request, Response } from "express";
+import { StatusCodes } from "http-status-codes";
+import { camelCase } from "lodash";
+
+export const get = async (req: Request, res: Response): Promise<Response<JSON>> => {
+    try {
+
+        const page: string = req.params.page;
+        const data = (await import("../../../data/www/pages/" + camelCase(page) + ".json")).default;
+
+        if (!data) {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+
+        if (page === "home" || page === "pricing") {
+            data.plans.slides = (await import("../../../data/www/plans.json")).default;
+        }
+
+        return res.status(StatusCodes.OK).json(data);
+
+    } catch (e: any) {
+
+        if (e.code === "MODULE_NOT_FOUND") {
+            return res.status(StatusCodes.NOT_FOUND).end();
+        }
+
+        throw e;
+
+    }
+}
